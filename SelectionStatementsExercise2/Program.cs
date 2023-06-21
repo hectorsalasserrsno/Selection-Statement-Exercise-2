8a02c66af8abc435a27b0d8344b597959c75d29f
Hector Salas Serrano <kenkira666@gmail.com>
2023-06-21 14:42:22 -0500

    Excercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is your favorite class?");
+            var asigment = Console.ReadLine();
+            Console.WriteLine("-----------------------------------");
+              
+            
+
+            switch (asigment) 
+            {
+
+                case "Algebra":
+
+                    Console.WriteLine("That is a boring class");
+
+                    break;
+
+                case "Geography":
+
+                    Console.WriteLine("That is an interesting class");
+
+                    break;
+
+                case "Spanish":
+
+                    Console.WriteLine("is a difficult class");
+
+                    break;
+
+                case "Japanesse":
+
+                    Console.WriteLine("that is my favorite class too");
+
+                    break;
+
+                case "Art":
+
+                    Console.WriteLine("I am into that class, I love to draw");
+
+                    break;
+
+                default:
+
+                    Console.WriteLine("well that is not what I ask");
+
+                    break;
+
+
+
+            }
         }
     }
 }
